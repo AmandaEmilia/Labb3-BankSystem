ce2a688fa8892fb9b57ad635fc551fcf2bdc0eed
Amanda Ström <amandastromiths@hotmail.com>
2023-02-20 13:30:25 +0100

    deposit, mocking bankservice

--- src/test/java/com/bank/BankMenuTest.java
+++ src/test/java/com/bank/BankMenuTest.java
@@ -1,7 +1,32 @@
 package com.bank;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.bank.service.BankService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Scanner;
+import static com.bank.TestUtil.getBankAccount;
 
 class BankMenuTest {
+
+    @Test
+    void shouldDeposit(){
+        BankService bankServiceMock = Mockito.mock(BankService.class);
+        BankAccount bankAccount = getBankAccount();
+        Scanner scMock = Mockito.mock(Scanner.class);
+
+        BankMenu bankMenu = new BankMenu(bankServiceMock, bankAccount, scMock);
+
+        Mockito.when(scMock.next()).thenReturn("b","e");
+
+        Mockito.when(scMock.nextDouble()).thenReturn(100d);
+
+        bankMenu.menu();
+
+        Mockito.verify(bankServiceMock, Mockito.times(1))
+                .deposit(bankAccount, 100d);
+    }
 
 }
